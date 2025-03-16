f6323d627c6a8ef640cdcc84bdb6679d6571b6cf
gtkacz <gmtkacz@proton.me>
2025-03-15 22:15:45 -0300

    Altering autodoc configs

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ version = pyproject['project']['version']
 sys.path.insert(0, os.path.abspath('..'))
 
 project = 'Temporal Adjuster'
-copyright = f'{date.today().year}, Gabriel Mitelman Tkacz'
+copyright = f'2024-{date.today().year}, Gabriel Mitelman Tkacz'
 author = 'Gabriel Mitelman Tkacz'
 version = version
 
@@ -50,15 +50,13 @@ napoleon_google_docstring = True
 napoleon_numpy_docstring = False
 napoleon_include_init_with_doc = False
 napoleon_include_private_with_doc = False
-napoleon_include_special_with_doc = True
+napoleon_include_special_with_doc = False
 napoleon_use_admonition_for_examples = False
 napoleon_use_admonition_for_notes = False
 napoleon_use_admonition_for_references = False
 napoleon_use_ivar = False
 napoleon_use_param = True
 napoleon_use_rtype = True
-napoleon_preprocess_types = True
-napoleon_type_aliases = None
 napoleon_attr_annotations = True
 
 # Autodoc settings
